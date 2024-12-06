7803203d520a6b940d7e63fb82e8fae1c6cfe901
Rudraksh Rawal <144530387+Rudrakshrawal@users.noreply.github.com>
2024-12-07 01:31:56 +0530

    shortened

--- fcfs.c
+++ fcfs.c
@@ -1,72 +1,30 @@
 #include <stdio.h>
 
-typedef struct {
-    int process_id;
-    int arrival_time;
-    int burst_time;
-    int completion_time;
-    int turnaround_time;
-    int waiting_time;
-} ps;
-
-void calculateTimes(ps processes[], int n) {
-    int total_waiting_time = 0, total_turnaround_time = 0;
-    
-    processes[0].completion_time = processes[0].arrival_time + processes[0].burst_time;
-    processes[0].turnaround_time = processes[0].completion_time - processes[0].arrival_time;
-    processes[0].waiting_time = processes[0].turnaround_time - processes[0].burst_time;
-    
-    total_waiting_time += processes[0].waiting_time;
-    total_turnaround_time += processes[0].turnaround_time;
-    
-    for (int i = 1; i < n; i++) {
-        processes[i].completion_time = processes[i-1].completion_time + processes[i].burst_time;
-        processes[i].turnaround_time = processes[i].completion_time - processes[i].arrival_time;
-        processes[i].waiting_time = processes[i].turnaround_time - processes[i].burst_time;
-
-        total_waiting_time += processes[i].waiting_time;
-        total_turnaround_time += processes[i].turnaround_time;
-    }
-    
-    printf("\nAverage Waiting Time: %.2f\n", (float)total_waiting_time / n);
-    printf("Average Turnaround Time: %.2f\n", (float)total_turnaround_time / n);
-}
-
-void printProcessDetails(ps processes[], int n) {
-    printf("\nProcess\tArrival Time\tBurst Time\tCompletion Time\tWaiting Time\tTurnaround Time\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n",
-               processes[i].process_id,
-               processes[i].arrival_time,
-               processes[i].burst_time,
-               processes[i].completion_time,
-               processes[i].waiting_time,
-               processes[i].turnaround_time);
-    }
-}
-
 int main() {
     int n;
-    
-    // Get number of processes
-    printf("Enter number of processes: ");
+    printf("Enter the number of processes: ");
     scanf("%d", &n);
-
-    ps processes[n];
-    
-    // Get ps details
+    int bt[n], at[n], wt[n], tat[n];
+    wt[0] = 0;
+    printf("Enter arrival time and burst time for each process:\n");
     for (int i = 0; i < n; i++) {
-        printf("\nEnter arrival time and burst time for ps %d:\n", i+1);
-        processes[i].process_id = i + 1;
-        printf("Arrival Time: ");
-        scanf("%d", &processes[i].arrival_time);
-        printf("Burst Time: ");
-        scanf("%d", &processes[i].burst_time);
+        printf("Process %d (Arrival Time, Burst Time): ", i + 1);
+        scanf("%d %d", &at[i], &bt[i]);
     }
-    
-    // Calculate times and print ps details
-    calculateTimes(processes, n);
-    printProcessDetails(processes, n);
+    // Calculate waiting time and turnaround time
+    for (int i = 1; i < n; i++) {
+        wt[i] = wt[i - 1] + bt[i - 1];  // Waiting time calculation
+        if (wt[i] < at[i]) wt[i] = at[i] - wt[i - 1]; // Adjust for arrival time
+    }
+
+    for (int i = 0; i < n; i++) {
+        tat[i] = wt[i] + bt[i];  // Turnaround time calculation
+    }
+    // Display results
+    printf("\nProcess\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("P%d\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, at[i], bt[i], wt[i], tat[i]);
+    }
 
     return 0;
 }
