cf6f6377015ddef7412c00e50c3b7c27b9a98cca
Rudraksh Rawal <144530387+Rudrakshrawal@users.noreply.github.com>
2024-12-07 01:45:08 +0530

    added library | | changed

--- meminfo.c
+++ meminfo.c
@@ -1,45 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<sys/sysinfo.h>
+#include <sys/sysinfo.h>
+#include <cstring>
 
 int main() {
     FILE *fp;
     char buffer[256];
     int status = 0 ;
     struct sysinfo un;
-	status = sysinfo(&un);
 
-    if(status == 0)
-    {
-        printf("\nMemTotal : %ld", (un.totalram));
-		printf("\nMemFree:: %ld", (un.freeram));
-		printf("\nMemAvail: %ld\n", (un.totalram-un.freeram));
+    status = sysinfo(&un);
+
+    if (status == 0) {
+        printf("\nMemTotal : %ld", un.totalram);
+        printf("\nMemFree:: %ld", un.freeram);
+        printf("\nMemAvail: %ld\n", un.totalram - un.freeram);
     }
 
     printf("------------------------\n");
 
-/* or you can read from /proc/meminfo*/
-
+    // Reading memory information from /proc/meminfo
     fp = fopen("/proc/meminfo", "r");
-    if (fp == NULL) 
-    {
+    if (fp == NULL) {
         perror("Error opening /proc/meminfo");
         return 1;
     }
 
     printf("Memory Information:\n");
-    
-    while (fgets(buffer, sizeof(buffer), fp) != NULL) 
-    {
-        
+
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
         if (strncmp(buffer, "MemTotal:", 9) == 0 ||
             strncmp(buffer, "MemFree:", 8) == 0 ||
-            strncmp(buffer, "MemAvailable:", 13) == 0) 
-        {
+            strncmp(buffer, "MemAvailable:", 13) == 0) {
             printf("%s", buffer);
         }
     }
-    
 
     fclose(fp);
 
